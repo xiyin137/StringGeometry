dfea2d273af4996fbbf38b669a67b5f9e6349541
Xi Yin <xiyin137@gmail.com>
2026-02-25 22:38:46 -0500

    Update umbrella dependency pins after README sync

--- lakefile.lean
+++ lakefile.lean
@@ -7,13 +7,13 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "master"
 
 require SGSupermanifolds from git
-  "https://github.com/xiyin137/stringgeometry-supermanifolds.git" @ "9565b88"
+  "https://github.com/xiyin137/stringgeometry-supermanifolds.git" @ "4d48fb0"
 
 require SGRiemannSurfaces from git
-  "https://github.com/xiyin137/stringgeometry-riemann-surfaces.git" @ "eb29eb7"
+  "https://github.com/xiyin137/stringgeometry-riemann-surfaces.git" @ "ab1b034"
 
 require SGSuperRiemannSurfaces from git
-  "https://github.com/xiyin137/stringgeometry-super-riemann-surfaces.git" @ "743a6c4"
+  "https://github.com/xiyin137/stringgeometry-super-riemann-surfaces.git" @ "b79f1ce"
 
 lean_lib StringGeometry where
   roots := #[`StringGeometry]
