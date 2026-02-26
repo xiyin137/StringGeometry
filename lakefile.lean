a79430d0174a892b6e351db2f870edd3f75b0968
Xi Yin <xiyin137@gmail.com>
2026-02-25 23:28:05 -0500

    Sync umbrella pins after supermanifolds quality-gate hardening

--- lakefile.lean
+++ lakefile.lean
@@ -7,13 +7,13 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "master"
 
 require SGSupermanifolds from git
-  "https://github.com/xiyin137/stringgeometry-supermanifolds.git" @ "fc8efce"
+  "https://github.com/xiyin137/stringgeometry-supermanifolds.git" @ "121ab54"
 
 require SGRiemannSurfaces from git
   "https://github.com/xiyin137/stringgeometry-riemann-surfaces.git" @ "62a9791"
 
 require SGSuperRiemannSurfaces from git
-  "https://github.com/xiyin137/stringgeometry-super-riemann-surfaces.git" @ "e9020d4"
+  "https://github.com/xiyin137/stringgeometry-super-riemann-surfaces.git" @ "6179767"
 
 lean_lib StringGeometry where
   roots := #[`StringGeometry]
